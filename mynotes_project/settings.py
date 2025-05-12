63d8d888e84d15b107d46c7b727ab5f0e7ae2069
MahmoudEissaM <mahmoudessa8200@gmail.com>
2025-05-12 17:53:02 +0300

    Add REST Framework to INSTALLED_APPS and configure REST Framework settings

--- mynotes_project/settings.py
+++ mynotes_project/settings.py
@@ -40,7 +40,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'notes.apps.NotesConfig',
     'widget_tweaks',
+    'rest_framework',
 ]
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
